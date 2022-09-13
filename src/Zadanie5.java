7d5346f3dec2e3d8fb0d45d263c992e7a66213bb
Przemek Golonka <golonkaprzemek57@gmail.com>
2022-09-13 12:37:14 +0200

    commit1

--- src/Zadanie5.java
+++ src/Zadanie5.java
@@ -1,14 +1,11 @@
 public class Zadanie5 {
     public static void main(String[] args) {
-        int PierwszePomieszczenie = 3 * 4;
-        int DrugiePomieszczenie = 3 * 4;
-        int TrzeciePomieszczenie = 2 * 3;
-        int CzwartePomieszczenie = 2 * 2;
-        int IloscOsob = 4;
-        System.out.println("1 Pomieszczenie ma:" + PierwszePomieszczenie + "metrow");
-        System.out.println("2 Pomieszczenie ma:" + DrugiePomieszczenie + "metrow");
-        System.out.println("3 Pomieszczenie ma:" + TrzeciePomieszczenie + "metrow");
-        System.out.println("4 Pomieszczenie ma:" + CzwartePomieszczenie + "metrow");
-        System.out.println("Całkowita powierzchnia mieszkania na jedna osobe" + (PierwszePomieszczenie + DrugiePomieszczenie + TrzeciePomieszczenie + CzwartePomieszczenie)/IloscOsob + " metrow");
+     int room1=4, room2=4, room3=3, room4=2;
+     int siana1=2, sciana2=3;
+     int powierzchniapomieszczenia1= room1*
+
+
+
+
     }
 }
